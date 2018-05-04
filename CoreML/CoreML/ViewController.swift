645a5249737a7ad5fe75853c13d7050fcd08e850
samira-daneshyar <samira.daneshyar@traderev.com>
2018-05-04 10:54:11 -0400

    First commit

---
+++ CoreML/CoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreML
+//
+//  Created by Samira Daneshyar on 5/4/18.
+//  Copyright © 2018 Samira Daneshyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
